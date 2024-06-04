940b50643feed2d790679b6e70573961c3ad7941
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-06-04 18:07:38 +0300

    fix : bug-fix in textCmponent

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+import HeroSection from "./components/hero/HeroSection";
+
+function App() {
+  return (
+    <div className="flex flex-col min-w-fit mx-auto">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
